60428ca7902ba6098fdeb29ca20afca1bb404a74
Ryan Blakley <rblakley@redhat.com>
2023-07-24 08:13:17 -0400

    Update the logger more to function better
    Signed-off-by: Ryan Blakley <rblakley@redhat.com>

--- planb/logger.py
+++ planb/logger.py
@@ -16,34 +16,38 @@ def set_log_cfg(opts, cfg):
     # Create the main logger.
     logger = logging.getLogger('pbr')
 
-    # Set the log level based on cfg file.
+    # Create the handlers.
+    log_con = logging.StreamHandler(sys.stdout)
+    log_file = logging.FileHandler('/var/log/pbr.log', mode='a')
+
+    # Set the format.
+    if opts.verbose:
+        con_format = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+    else:
+        con_format = logging.Formatter('%(message)s')
+
+    log_con.setFormatter(con_format)
+    log_file.setFormatter(logging.Formatter('%(asctime)s - PBR - %(levelname)s: %(message)s'))
+
+    # Set the log level based on cfg file and arguments.
     if cfg.log_verbosity == "debug":
         logger.setLevel(logging.DEBUG)
+        log_file.setLevel(logging.DEBUG)
+        if opts.verbose:
+            log_con.setLevel(logging.DEBUG)
+        else:
+            log_con.setLevel(logging.INFO)
     elif cfg.log_verbosity == "info":
         if opts.verbose:
             logger.setLevel(logging.DEBUG)
+            log_con.setLevel(logging.DEBUG)
+            log_file.setLevel(logging.DEBUG)
         else:
             logger.setLevel(logging.INFO)
+            log_con.setLevel(logging.INFO)
+            log_file.setLevel(logging.INFO)
 
-    # Create the log formatter.
-    log_format = logging.Formatter('%(asctime)s - PBR - %(levelname)s: %(message)s')
-
-    # Create console handler and set level depending opts.verbose.
-    log_con = logging.StreamHandler(sys.stdout)
-    if opts.verbose:
-        con_format = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
-        log_con.setLevel(logging.DEBUG)
-    else:
-        con_format = logging.Formatter('%(message)s')
-        log_con.setLevel(logging.INFO)
-
-    log_con.setFormatter(con_format)
     logger.addHandler(log_con)
-
-    # Create log file handler and the log level will be set above
-    # based on what the cfg file log level is set to.
-    log_file = logging.FileHandler('/var/log/pbr.log', mode='a')
-    log_file.setFormatter(log_format)
     logger.addHandler(log_file)
 
     return logger
